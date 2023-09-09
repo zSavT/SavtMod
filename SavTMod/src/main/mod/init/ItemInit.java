85601315b6505f05b6e5c57d9e32da501b2e63e5
zSavT <francesco.saverio.cassano@gmail.com>
2023-09-09 10:44:00 +0200

    base

---
+++ SavTMod/src/main/mod/init/ItemInit.java
@@ -0,0 +1,8 @@
+
+
+public class ItemInit extends  Item {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, FirstModMain.MOD_ID);
+}
+
+
